d3828eaea8632982e99b4d06ddcb92374255104a
Rupesh Chidupudi <chrupesh2425@gmail.com>
2024-09-01 22:21:35 +0530

    changes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -8,6 +8,7 @@ const Welcome = () => {
             <header className="welcome-header">
                 <img src="your-logo-url" alt="Logo" className="logo" />
                 <nav>
+                    
                     <ul className="nav-links">
                         <li><Link to="/services">Services</Link></li>
                         <li><Link to="/solutions">Solutions</Link></li>
